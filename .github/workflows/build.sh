b2a14e5a5406032c424bca20c6f39bbb35b99f56
marci <marcel@secure-bits.org>
2025-07-21 14:21:45 +0200

    Korrigiere Download-Links und Dateinamen für Hugo im Build-Skript

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,10 +7,10 @@ main() {
 
   # Install Hugo
   echo "Installing Hugo version $Hugo_VERSION..."
-  curl -LJO "https://github.com/gohugoio/hugo/releases/download/v$Hugo_VERSION/hugo_extended_$Hugo_VERSION_Linux-64bit.tar.gz"
-  tar -xzf "hugo_extended_$Hugo_VERSION_Linux-64bit.tar
+  curl -LJO "https://github.com/gohugoio/hugo/releases/download/v$Hugo_VERSION/hugo_extended_$Hugo_VERSION_linux-amd64.tar.gz"
+  tar -xzf "hugo_extended_$Hugo_VERSION_linux-amd64.tar 
   cp hugo /opt/buildhome
-  rm LICENSE.txt README.md hugo_extended_$Hugo_VERSION_Linux-64bit.tar.gz
+  rm LICENSE.txt README.md hugo_extended_$Hugo_VERSION_linux-amd64.tar.gz
 
   echo "Hugo version $Hugo_VERSION installed successfully."
   echo Go: "${go version}"
