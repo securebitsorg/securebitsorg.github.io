28386426023b0e1608803cdccb59e14816377afa
marci <marcel@secure-bits.org>
2025-07-21 18:53:17 +0200

    Korrigiere Tippfehler in der wrangler.toml und optimiere das Build-Skript für Hugo-Installation

--- build.sh
+++ build.sh
@@ -12,45 +12,44 @@
 
 main() {
 
-    DART_SASS_VERSION=1.89.2
-    HUGO_VERSION=0.148.1
+  DART_SASS_VERSION=1.89.2
+  HUGO_VERSION=0.148.0
 
-    export TZ=Europe/Oslo
+  export TZ=Europe/Oslo
 
-    # Install Dart Sass
-    echo "Installing Dart Sass v${DART_SASS_VERSION}..."
-    curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-    tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-    cp -r dart-sass/ /opt/buildhome
-    rm -rf dart-sass*
+  # Install Dart Sass
+  echo "Installing Dart Sass v${DART_SASS_VERSION}..."
+  curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  cp -r dart-sass/ /opt/buildhome
+  rm -rf dart-sass*
 
+  # Install Hugo
+  echo "Installing Hugo v${HUGO_VERSION}..."
+  curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+  tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  cp hugo /opt/buildhome
+  rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 
-    # Install Hugo
-    echo "Installing Hugo v${HUGO_VERSION}..."
-    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}-linux-amd64.tar.gz
-    tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-    cp hugo /opt/buildhome
-    rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+  # Set PATH
+  echo "Setting the PATH environment variable..."
+  export PATH=/opt/buildhome:/opt/buildhome/dart-sass:$PATH
 
-    # Set PATH
-    echo "Setting the PATH environment variable..."
-    export PATH=/opt/buildhome:/opt/buildhome/dart-sass:$PATH
+  # Verify installed versions
+  echo "Verifying installations..."
+  echo Dart Sass: "$(sass --version)"
+  echo Go: "$(go version)"
+  echo Hugo: "$(hugo version)"
+  echo Node.js: "$(node --version)"
 
-    # Verify installed versions
-    echo "Verifying installations..."
-    echo Dart Sass: "$(sass --version)"
-    echo Go: "$(go version)"
-    echo Hugo: "$(hugo version)"
-    echo Node.js: "$(node --version)"
+  # https://gohugo.io/methods/page/gitinfo/#hosting-considerations
+  git fetch --recurse-submodules --unshallow
 
-    # https://gohugo.io/methods/page/gitinfo/#hosting-considerations
-    git fetch --recurse-submodules --unshallow
+  # https://github.com/gohugoio/hugo/issues/9810
+  git config core.quotepath false
 
-    # https://github.com/gohugoio/hugo/issues/9810
-    git config core.quotepath false
-
-    # Build the site.
-    hugo --gc --minify
+  # Build the site.
+  hugo --gc --minify
 
 }
 
