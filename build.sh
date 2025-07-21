11bbf5a30f330acd27eb65e2c8242d4aa30c4d48
marci <marcel@secure-bits.org>
2025-07-21 19:00:36 +0200

    update bash.sh

--- build.sh
+++ build.sh
@@ -12,18 +12,12 @@
 
 main() {
 
-  DART_SASS_VERSION=1.89.2
+  
   HUGO_VERSION=0.148.0
 
   export TZ=Europe/Oslo
 
-  # Install Dart Sass
-  echo "Installing Dart Sass v${DART_SASS_VERSION}..."
-  curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-  tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-  cp -r dart-sass/ /opt/buildhome
-  rm -rf dart-sass*
-
+ 
   # Install Hugo
   echo "Installing Hugo v${HUGO_VERSION}..."
   curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
