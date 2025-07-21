30347afb85113c4f3f4c5d22318497e653704632
marci <marcel@secure-bits.org>
2025-07-21 14:04:32 +0200

    Füge Build- und Wrangler-Konfigurationsdateien hinzu

---
+++ .github/workflows/build.sh
@@ -0,0 +1,29 @@
+#1/usr/bin/env bash
+
+main() {
+  Hugo_VERSION="0.148.1"
+
+  export TZ="Europe/Berlin"
+
+  # Install Hugo
+  echo "Installing Hugo version $Hugo_VERSION..."
+  curl -LJO "https://github.com/gohugoio/hugo/releases/download/v$Hugo_VERSION/hugo_extended_$Hugo_VERSION_Linux-64bit.tar.gz"
+  tar -xzf "hugo_extended_$Hugo_VERSION_Linux-64bit.tar
+  cp hugo /opt/buildhome
+  rm LICENSE.txt README.md hugo_extended_$Hugo_VERSION_Linux-64bit.tar.gz
+
+  echo "Hugo version $Hugo_VERSION installed successfully."
+  echo Go: "${go version}"
+  echo "Hugo version: $(hugo version)"
+  echo Node.js version: "$(node -v)"
+
+  echo "Clone Toha theme..."
+  git submodule update --init --recursive
+  git config core.quotepath false
+
+  echo "Building site..."
+  hugo --gc --minify
+}
+
+set -euo pipefail
+main "$@"
