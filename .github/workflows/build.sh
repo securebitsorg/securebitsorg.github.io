4a0e3646c5db3d33812d078ba2e72f2fe7112385
marci <marcel@secure-bits.org>
2025-07-21 14:38:01 +0200

    Füge Installation von Dart Sass und PATH-Setzung zum Build-Skript hinzu

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,8 +2,16 @@
 
 main() {
   Hugo_VERSION="0.148.1"
+  DART_SASS_VERSION="1.89.2"
 
   export TZ="Europe/Berlin"
+
+  # Install Dart Sass
+  echo "Installing Dart Sass v${DART_SASS_VERSION}..."
+  curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  cp -r dart-sass/ /opt/buildhome
+  rm -rf dart-sass*
 
   # Install Hugo
   echo "Installing Hugo version $Hugo_VERSION..."
@@ -12,7 +20,12 @@ main() {
   cp hugo /opt/buildhome
   rm LICENSE.txt README.md hugo_extended_$Hugo_VERSION_linux-amd64.tar.gz
 
+  # Set PATH
+  echo "Setting the PATH environment variable..."
+  export PATH=/opt/buildhome:/opt/buildhome/dart-sass:$PATH
+
   echo "Hugo version $Hugo_VERSION installed successfully."
+  echo Dart Sass: "$(sass --version)"
   echo Go: "${go version}"
   echo "Hugo version: $(hugo version)"
   echo Node.js version: "$(node -v)"    
