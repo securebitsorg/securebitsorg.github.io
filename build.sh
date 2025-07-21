fa5035f440cb15abae9fb5cd07c046936d7bf4a2
marci <marcel@secure-bits.org>
2025-07-21 18:49:17 +0200

    Entferne Go-Installation aus dem Build-Skript und korrigiere die URL für die Hugo-Installation; aktualisiere das Datumsformat in der wrangler.toml

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ main() {
 
     DART_SASS_VERSION=1.89.2
     HUGO_VERSION=0.148.1
-    GO_VERSION=1.24.4
 
     export TZ=Europe/Oslo
 
@@ -25,15 +24,10 @@ main() {
     cp -r dart-sass/ /opt/buildhome
     rm -rf dart-sass*
 
-    # Install Go
-    echo "Installing Go v${GO_VERSION}..."
-    curl -LJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-    tar -xf "go${GO_VERSION}.linux-amd64.tar.gz" -C /opt
-    rm "go${GO_VERSION}.linux-amd64.tar.gz"
 
     # Install Hugo
     echo "Installing Hugo v${HUGO_VERSION}..."
-    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}-linux-amd64.tar.gz
     tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
     cp hugo /opt/buildhome
     rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
